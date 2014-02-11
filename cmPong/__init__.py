adb0004a9931f5a9936cd4ccb3ec5b175be12f4b
Kristen Thyng <kthyng@gmail.com>
2014-02-11 16:52:08 -0600

    Got everything sorted out for using cmPong as a package.
    Added setup.py and __init.py files. README has details for use.

---
+++ cmPong/__init__.py
@@ -0,0 +1,8 @@
+'''
+cmPong is a set of colormaps that the Physical Oceanography Numerical Group
+(PONG) at Texas A&M University uses for plots.
+'''
+
+import cmPong
+
+__authors__ = ['Kristen Thyng <kthyng@tamu.edu>']
--- setup.py
+++ setup.py
@@ -1,60 +1,26 @@
 #!/usr/bin/env python
 
 """
-setup.py for tracpy -- python wrappers around TRACMASS
+setup.py for cmPong
 
 """
 import shutil
 from setuptools import setup # to support "develop" mode
 from numpy.distutils.core import setup, Extension
 
-# ## total kludge:
-# ## this copies extension built with makefile to package
-# ## better to have setup.py build TRACMASS
-
-# try:
-#     shutil.copyfile("src/tracmass.so", "tracpy/tracmass.so")
-
-# except IOError:
-#     raise Exception("TRAMASS extesnion not built -- built it with the makefile in src")
-
-tracmass_mod = Extension(name = "tracmass",
-                         sources=['src/calc_dxyz.f95',
-                                  'src/cross.f95',
-                                  'src/loop_pos.f95',
-                                  'src/step.f95',
-                                  'src/vertvel.f95',
-                                  'src/calc_time.f95',
-                                  'src/diffusion.f95',
-                                  'src/pos.f95',
-                                  'src/turb.f95',
-                                  ],
-                      #extra_f90_compile_args=["-ffixed-form"],
-                      )
-
-# modules = ["tracpy/inout.py",
-#            "tracpy/op.py"
-#            "tracpy/plotting.py",
-#            "tracpy/run.py",
-#            "tracpy/tools.py",
-#            ]
-
-print tracmass_mod
-
 setup(
-    name = "tracpy",
+    name = "cmPong",
     version = "0.01",
     author = "Kristen Thyng",
     author_email = "",
-    description = ("python wrappers around TRACMASS"),
+    description = ("Colormaps for PONG"),
     long_description=open('README.md').read(),
     classifiers=[
                  "Development Status :: 3 - Alpha",
     #             "Topic :: Utilities",
                  ],
-    packages = ["tracpy"],
+    packages = ["cmPong"],
     # py_modules = modules,
-    ext_package='tracpy', 
-    ext_modules = [tracmass_mod],
+    ext_package='cmPong', 
     scripts = [],
     )
